0052f320506543e087e4331a51ba6cb6636241f9
pszponder <14116413+pszponder@users.noreply.github.com>
2022-02-13 20:52:06 -0700

    Connect MongoDB database to server and add middleware for parsing data from HTTP requests

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,55 @@
-// IMPORT AND LOAD ENVIRONMENT VARIABLES
+// ===================
+// IMPORT DEPENDENCIES
+// ===================
+
+const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Custom Middleware
+const { errorHandler } = require('./middleware/errorHandler');
+
+// ==========================
+// LOAD ENVIRONMENT VARIABLES
+// ==========================
 dotenv.config(); // Load .env file contents into process.env.
 
-// IMPORT AND INITIALIZE CONNECTION TO MONGODB SERVER
-const connectDB = require('./config/db');
+// =======================================
+// INITIALIZE CONNECTION TO MONGODB SERVER
+// =======================================
 connectDB();
 
-// IMPORT AND INITIALIZE EXPRESS
-const express = require('express');
+// ==================
+// INITIALIZE EXPRESS
+// ==================
 const app = express();
 
-// IMPORT AND INITIALIZE 3rd PARTY NODE MODULES / MIDDLEWARE
+// ==============================================
+// INITIALIZE 3rd PARTY NODE MODULES / MIDDLEWARE
+// ==============================================
 
-// IMPORT AND INITIALIZE CUSTOM MODULES / MIDDLEWARE
+// Middleware to parse JSON or urlencoded payloads from body data in request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// =====================
+// SETUP ROUTE ENDPOINTS
+// =====================
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// ======================================
+// INITIALIZE CUSTOM MODULES / MIDDLEWARE
+// ======================================
+
+// Overwrite Node's error handling
+app.use(errorHandler);
+
+// ======================================
 // ESTABLISH PORT FOR SERVER TO LISTEN ON
+// ======================================
 const PORT = process.env.PORT || 5000;
 
+// ==============
 // LISTEN TO PORT
+// ==============
 app.listen(PORT, () => console.log(`Server listening on port ${PORT} ...`));
