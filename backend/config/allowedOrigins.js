7521c26eabd0bb49f2d2c923f46886d874999807
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-11 19:17:54 -0700

    Add localhost:3000 to allowed CORS list
    * Also cleaned up comments

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,15 +1,8 @@
-// ==========================
-// LOAD ENVIRONMENT VARIABLES
-// ==========================
-// const dotenv = require('dotenv');
-// dotenv.config(); // Load .env file contents into process.env
-
 // ===========================================
 // DEFINE AN ARRAY OF URI FOR CORS PERMISSIONS
 // ===========================================
 
-// const allowedOrigins = [`http://localhost:${process.env.PORT}`];
-const allowedOrigins = [`http://localhost:5000`];
+const allowedOrigins = [`http://localhost:5000`, 'http://localhost:3000'];
 
 // ========================
 // EXPORT THE ARRAY OF URIs
