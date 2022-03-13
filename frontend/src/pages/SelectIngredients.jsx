41e77e3d04959cf8cb65bdc79085e6fc06873566
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-12 20:53:46 -0700

    Saving in process work (need to fix SAVE TO DB BUTTON)

--- frontend/src/pages/SelectIngredients.jsx
+++ frontend/src/pages/SelectIngredients.jsx
@@ -20,6 +20,9 @@ function SelectIngredients() {
           withCredentials: true,
         });
 
+        // TODO: REMOVE
+        console.log('ingredientList :>> ', ingredientList);
+
         // Update the ingredientList
         setIngredientList(response.data);
       } catch (error) {
@@ -32,6 +35,9 @@ function SelectIngredients() {
         const response = await axios.get('/api/users/ingredients', {
           withCredentials: true,
         });
+
+        // TODO: REMOVE
+        console.log('userIngredientList :>> ', userIngredientList);
         // Update the ingredientList
         setUserIngredientList(response.data);
       } catch (error) {
@@ -43,41 +49,100 @@ function SelectIngredients() {
     getUserIngredientList();
   }, []);
 
-  // Call the API to add an array of ingredients to the user's list of ingredients
-  // useEffect((ingredientIds) => {
-  //   const addIngredients = async (ingredientIds) => {
+  // // Call the API to add an array of ingredients to the user's list of ingredients
+  // useEffect(() => {
+  //   const ingredientIds = userIngredientList.map(
+  //     (ingredient) => ingredient._id
+  //   );
+  //   console.log('ingredientIds', ingredientIds);
+
+  //   const removeAllIngredients = async () => {
   //     try {
-  //       const response = await axios.post('/api/users/ingredients', ingredientIds, {
+  //       const response = await axios.delete('/api/users/ingredients', {
   //         withCredentials: true,
   //       });
-
-  //       // Update the ingredientList
-  //       setIngredientList(response.data);
   //     } catch (error) {
   //       console.log(`Error: ${error.message}`);
   //     }
   //   };
-  // }, [userIngredientList])
 
-  const handleAddIngredient = (ingredientId) => {
-    setUserIngredientList((prevState) => [...prevState, ingredientId]);
+  //   const addIngredients = async () => {
+  //     try {
+  //       const response = await axios.post(
+  //         '/api/users/ingredients',
+  //         { ingredientIds },
+  //         {
+  //           withCredentials: true,
+  //         }
+  //       );
+  //     } catch (error) {
+  //       console.log(`Error: ${error.message}`);
+  //     }
+  //   };
+  //   removeAllIngredients();
+  //   addIngredients();
+  // }, [userIngredientList]);
+
+  const handleAddIngredient = (ingredient) => {
+    // Check that ingredient isn't already in user's list
+    const idx = userIngredientList.findIndex(
+      (userIngredient) =>
+        userIngredient.ingredientName === ingredient.ingredientName
+    );
+
+    // Add ingredient to user's list if it does not exist
+    if (idx === -1) {
+      setUserIngredientList((prevState) => [...prevState, ingredient]);
+    }
   };
 
-  const handleRemoveIngredient = (ingredientId) => {
+  const handleRemoveIngredient = (ingredient) => {
     // Create a filtered array without the ingredientId
+    const filteredIngredients = userIngredientList.filter(
+      (userIngredient) => userIngredient._id !== ingredient._id
+    );
 
-    setUserIngredientList();
+    // Update the User's ingredient List with the filtered list
+    setUserIngredientList(filteredIngredients);
   };
 
-  const mapList = (list) => {
-    return list.map((ingredient) => (
-      <li key={ingredient._id}>
-        <h4>{ingredient.ingredientName}</h4>
-        <p>{ingredient.foodGroup}</p>
-        <p>{ingredient._id}</p>
-        <button>Add to User's List</button>
-      </li>
-    ));
+  // TODO: DOES THIS NEED TO BE USEEFFECT?
+  const handleSave = () => {
+    console.log('TODO: FIX ME, NOT UPDATING DB PROPERLY');
+    const ingredientIds = userIngredientList.map(
+      (ingredient) => ingredient._id
+    );
+    console.log('ingredientIds', ingredientIds);
+
+    const removeAllIngredients = async () => {
+      try {
+        const response = await axios.delete('/api/users/ingredients', {
+          withCredentials: true,
+        });
+        console.log('RESPONSE FROM REMOVING INGREDIENTS', response);
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+      }
+    };
+
+    const addIngredients = async () => {
+      console.log(ingredientIds);
+      try {
+        const response = await axios.post(
+          '/api/users/ingredients',
+          { ingredientIds },
+          {
+            withCredentials: true,
+          }
+        );
+        console.log('RESPONSE FROM ADDING INGREDIENTS', response);
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+      }
+    };
+
+    removeAllIngredients();
+    addIngredients();
   };
 
   return (
@@ -85,15 +150,39 @@ function SelectIngredients() {
       {/* INGREDIENTS LIST */}
       <section>
         <h2>INGREDIENTS LIST</h2>
-        <ul>{mapList(ingredientList)}</ul>
+        {/* <ul>{mapList(ingredientList)}</ul> */}
+        <ul>
+          {ingredientList.map((ingredient) => (
+            <li key={ingredient._id}>
+              <h4>{ingredient.ingredientName}</h4>
+              <p>{ingredient.foodGroup}</p>
+              <p>{ingredient._id}</p>
+              <button onClick={() => handleAddIngredient(ingredient)}>
+                Add to User's List
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
       {/* USER'S INGREDIENTS LIST */}
       <section>
         <h2>USER'S INGREDIENTS LIST</h2>
-        <ul>{mapList(userIngredientList)}</ul>
+        <ul>
+          {userIngredientList.map((ingredient) => (
+            <li key={ingredient._id}>
+              <h4>{ingredient.ingredientName}</h4>
+              <p>{ingredient._id}</p>
+              <button onClick={() => handleRemoveIngredient(ingredient)}>
+                Remove from User's List
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
       {/* SAVE USER'S CURRENT INGREDIENTS LIST TO DB */}
-      <button>SAVE</button>
+      <section>
+        <button onClick={handleSave}>SAVE TO DB</button>
+      </section>
     </div>
   );
 }
