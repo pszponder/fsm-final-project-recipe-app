dc8530e2f3a491658844b47c3b89bb121fee2dab
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-13 19:26:19 -0600

    Add initial logic to RecipeList

--- frontend/src/pages/RecipeList.jsx
+++ frontend/src/pages/RecipeList.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from '../api/axios';
+import DataContext from '../context/DataProvider';
 
 function RecipeList() {
-  return <div>RecipeList</div>;
+  // Extract the state and setter shared from the DataContext
+  const { recipeList, setRecipeList } = useContext(DataContext);
+
+  // Call the API to extract recipe list on component load
+  //  (pass in empty array as 2nd argument)
+  useEffect(() => {
+    const getAllRecipes = async () => {
+      try {
+        const response = await axios.get('/api/recipes');
+
+        // Update the recipeList
+        setRecipeList(response.data);
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+      }
+    };
+
+    getAllRecipes();
+  }, []);
+
+  return (
+    <div>
+      {/* RECIPE LIST */}
+      <section>
+        <h2>RECIPES LIST</h2>
+        <ul>
+          {recipeList.map((recipe) => (
+            <li key={recipe._id}>
+              <h4>{recipe.recipeName}</h4>
+              <p>{recipe.recipeDescription}</p>
+              <p>{recipe._id}</p>
+              <button>View Recipe</button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default RecipeList;
