4ccbeaec5ada8f8566bb1725ae2c54f4eef34b96
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-14 20:07:38 -0600

    Add initial logic to RecipeDetail
    * Display Recipe information
    * Back button to go back to Recipe List

--- frontend/src/pages/RecipeDetail.jsx
+++ frontend/src/pages/RecipeDetail.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function RecipeDetail() {
-  return <div>RecipeDetail</div>;
+  // Extract the state which was passed into the location object by useNavigate
+  const { state } = useLocation();
+  console.log(state);
+
+  return (
+    <div>
+      <Link to="/recipe-list">Back</Link>
+      <h1>{state.recipeName}</h1>
+      <h2>{state.recipeDescription}</h2>
+      <ul>
+        {state.recipeIngredients.map((ingredient, idx) => (
+          <li key={`ingredient${idx}`}>
+            {`${ingredient.ingredientName}: ${ingredient.ingredientQty} ${ingredient.ingredientUnitOfMeasurement}`}
+          </li>
+        ))}
+      </ul>
+      <ol>
+        {state.recipeDirections.map((direction, idx) => (
+          <li key={`direction${idx}`}>{direction}</li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default RecipeDetail;
