a7a0499606a2fdda9593164bb0ac8bc67de60948
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-14 19:15:54 -0600

    Add ability to route to recipe-detail component from recipe-list view recipe button

--- frontend/src/pages/RecipeList.jsx
+++ frontend/src/pages/RecipeList.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 import DataContext from '../context/DataProvider';
 
 function RecipeList() {
   // Extract the state and setter shared from the DataContext
   const { recipeList, setRecipeList } = useContext(DataContext);
+
+  // Create a navigate function to navigate to Recipe Detail
+  const navigateRecipeDetail = useNavigate();
+
+  // Create a function which will navigate to the Recipe Detail
+  const handleClick = (recipe) => {
+    navigateRecipeDetail('/recipe-detail', { state: recipe });
+  };
 
   // Call the API to extract recipe list on component load
   //  (pass in empty array as 2nd argument)
@@ -34,7 +43,7 @@ function RecipeList() {
               <h4>{recipe.recipeName}</h4>
               <p>{recipe.recipeDescription}</p>
               <p>{recipe._id}</p>
-              <button>View Recipe</button>
+              <button onClick={() => handleClick(recipe)}>View Recipe</button>
             </li>
           ))}
         </ul>
