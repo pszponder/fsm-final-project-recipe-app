22216f40ed06e92f8704ad2b0c2a0b8ce828b5a2
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-16 17:51:29 -0600

    Add filter function to conrol display of recipes
    * Only show recipies where user has at least partial ingredients for in their ingreditns list

--- frontend/src/pages/RecipeList.jsx
+++ frontend/src/pages/RecipeList.jsx
@@ -5,7 +5,12 @@ import DataContext from '../context/DataProvider';
 
 function RecipeList() {
   // Extract the state and setter shared from the DataContext
-  const { recipeList, setRecipeList } = useContext(DataContext);
+  const {
+    recipeList,
+    setRecipeList,
+    userIngredientList,
+    setUserIngredientList,
+  } = useContext(DataContext);
 
   // Create a navigate function to navigate to Recipe Detail
   const navigateRecipeDetail = useNavigate();
@@ -14,6 +19,9 @@ function RecipeList() {
   const handleClick = (recipe) => {
     navigateRecipeDetail('/recipe-detail', { state: recipe });
   };
+
+  // // Convert the name of the
+  // const userIngredientsNames = userIngredientList.map(ingredient => ingredient.ingredientName);
 
   // Call the API to extract recipe list on component load
   //  (pass in empty array as 2nd argument)
@@ -32,20 +40,40 @@ function RecipeList() {
     getAllRecipes();
   }, []);
 
+  const matchRecipe = (recipe) => {
+    // Extract the recipeIngredient names into an array
+    const recipeIngredientNames = recipe.recipeIngredients.map(
+      (ingredient) => ingredient.ingredientName
+    );
+
+    for (const recipeIngredientName of recipeIngredientNames) {
+      for (const userIngredientName of userIngredientList) {
+        console.log(recipeIngredientName, userIngredientName.ingredientName);
+        if (recipeIngredientName === userIngredientName.ingredientName) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
   return (
     <div>
       {/* RECIPE LIST */}
       <section>
         <h2>RECIPES LIST</h2>
         <ul>
-          {recipeList.map((recipe) => (
-            <li key={recipe._id}>
-              <h4>{recipe.recipeName}</h4>
-              <p>{recipe.recipeDescription}</p>
-              <p>{recipe._id}</p>
-              <button onClick={() => handleClick(recipe)}>View Recipe</button>
-            </li>
-          ))}
+          {recipeList.map((recipe) => {
+            return matchRecipe(recipe) ? (
+              <li key={recipe._id}>
+                <h4>{recipe.recipeName}</h4>
+                <p>{recipe.recipeDescription}</p>
+                <p>{recipe._id}</p>
+                <button onClick={() => handleClick(recipe)}>View Recipe</button>
+              </li>
+            ) : null;
+          })}
         </ul>
       </section>
     </div>
