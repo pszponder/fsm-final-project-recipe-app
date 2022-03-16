e7b4ae34ebae46f69b6f995726eb7e337c267185
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-15 21:40:46 -0600

    Add error validation and login redirect
    * User is redirected to login page on successful registration
    * If registration is unsuccessful, user is prompted with an error message

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import axios from '../api/axios';
 
 function Register() {
@@ -10,16 +11,27 @@ function Register() {
     password: '',
   });
 
+  const [registerError, setRegisterError] = useState(false);
+  const [registerSuccess, setRegisterSuccess] = useState(false);
+
+  // Change registerSuccess back to false on component unmount
+  useEffect(() => {
+    return () => {
+      setRegisterSuccess(false);
+    };
+  }, []);
+
   // Since we will be using axios to send asynchronous events
   //  to the backend, add the async keyword to handleSubmit
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('/api/auth/register', registerData);
-      // TODO: DO SOMETHING WITH RESPONSE
-      console.log(response.data);
+      await axios.post('/api/auth/register', registerData);
+      setRegisterError(false);
+      setRegisterSuccess(true);
     } catch (error) {
       console.log(error);
+      setRegisterError(true);
     }
   };
 
@@ -31,79 +43,72 @@ function Register() {
   };
 
   return (
-    <div>
-      {/* HEADER SECTION */}
-      <section>
-        <h1>REGISTER</h1>
-        <p>Please Create an Account</p>
-      </section>
-      {/* FORM SECTION*/}
-      <section>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={registerData.firstName}
-              placeholder="First Name"
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={registerData.lastName}
-              placeholder="Last Name"
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              value={registerData.email}
-              placeholder="Email"
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="password"
-              name="password"
-              value={registerData.password}
-              placeholder="Password"
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <button type="submit">SUBMIT</button>
-          </div>
-        </form>
-      </section>
-      {/* TEST FIELD */}
-      <section>
-        <hr />
+    <>
+      {registerSuccess ? (
+        <Navigate to="/login" />
+      ) : (
         <div>
-          <h4>
-            First Name: <span>{registerData.firstName}</span>
-          </h4>
-          <h4>
-            Last Name: <span>{registerData.lastName}</span>
-          </h4>
-          <h4>
-            Email: <span>{registerData.email}</span>
-          </h4>
-          <h4>
-            Password: <span>{registerData.password}</span>
-          </h4>
+          {/* HEADER SECTION */}
+          <section>
+            <h1>REGISTER</h1>
+            <p>Please Create an Account</p>
+          </section>
+          {/* FORM SECTION*/}
+          <section>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={registerData.firstName}
+                  placeholder="First Name"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={registerData.lastName}
+                  placeholder="Last Name"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={registerData.email}
+                  placeholder="Email"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={registerData.password}
+                  placeholder="Password"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <button type="submit">SUBMIT</button>
+              </div>
+            </form>
+          </section>
         </div>
+      )}
+      <section>
+        {registerError ? (
+          <h2>Please enter the correct register information</h2>
+        ) : null}
       </section>
-    </div>
+    </>
   );
 }
 
