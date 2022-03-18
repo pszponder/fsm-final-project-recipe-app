712b6a2120d2b171f9d9ce515cd69699d489f77a
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-18 17:43:01 -0600

    Add bootstrap components to header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <>
-      <header>
-        <div>
-          <Link to="/">What's Cooking?</Link>
-        </div>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
+    <div className="container">
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        >
+          <span className="fs-4">What's Cooking?</span>
+        </a>
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
           </li>
-          <li>
-            <Link to="/register">Register</Link>
+          <li className="nav-item">
+            <NavLink to="/recipe-list" className="nav-link">
+              Recipe List
+            </NavLink>
           </li>
-          <li>
-            <Link to="/logout">Logout</Link>
+          <li className="nav-item">
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
           </li>
-          <li>
-            <Link to="/">Main Page</Link>
+          <li className="nav-item">
+            <NavLink to="/register" className="nav-link">
+              Register
+            </NavLink>
           </li>
-          <li>
-            <Link to="/recipe-list">Recipe List</Link>
+          <li className="nav-item">
+            <NavLink to="/logout" className="nav-link">
+              Logout
+            </NavLink>
           </li>
         </ul>
       </header>
-      <hr />
-    </>
+    </div>
   );
 }
 
