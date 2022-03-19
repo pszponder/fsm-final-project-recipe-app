575a659bd71669f03ef7350264b81138f4fe254c
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-18 19:31:43 -0600

    Add bootstrap components to the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 
 function Footer() {
   return (
-    <>
-      <hr />
-      <div>Footer - Full Stack Mastery Bootcamp: Final Project</div>
-    </>
+    <div className="container fixed-bottom">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-muted">
+          Full Stack Mastery Bootcamp Final Project
+        </p>
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a
+              href="https://github.com/pszponder/fsm-final-project-recipe-app/tree/develop"
+              className="nav-link px-2 text-muted"
+            >
+              Project on GitHub
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
   );
 }
 
