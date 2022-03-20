2a6d1407737549805a81d9e3bee56e8642ee862f
pszponder <14116413+pszponder@users.noreply.github.com>
2022-03-20 14:29:34 -0600

    Redirect to login page if user is not logged in
    * Also removed unnecessary files

--- frontend/src/pages/SelectIngredients.jsx
+++ frontend/src/pages/SelectIngredients.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import axios from '../api/axios';
 import AuthContext from '../context/AuthProvider';
 import DataContext from '../context/DataProvider';
-import { useNavigate, Link } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 
 function SelectIngredients() {
   // Extract the state and setter shared from the DataContext
@@ -14,14 +14,6 @@ function SelectIngredients() {
     userIngredientList,
     setUserIngredientList,
   } = useContext(DataContext);
-
-  // Create a navigate function to navigate to Recipe Detail
-  const navigateRecipeList = useNavigate();
-
-  // Create a function which will navigate to the Recipe Detail
-  const handleClick = (recipeList) => {
-    navigateRecipeList('/recipe-list', { state: recipeList });
-  };
 
   // Call the API to extract ingredients and user ingredients list
   //  on component load (pass in empty array as 2nd argument)
@@ -82,7 +74,7 @@ function SelectIngredients() {
 
     const updateAllIngredients = async () => {
       try {
-        const response = await axios.put('/api/users/ingredients', {
+        await axios.put('/api/users/ingredients', {
           ingredientIds,
         });
       } catch (error) {
@@ -93,78 +85,84 @@ function SelectIngredients() {
     updateAllIngredients();
   };
 
-  
- 
-
   return (
     <div>
-      {/* INGREDIENTS LIST */}
-      <article className="ingredients-list">
-        <h2 className="ingredients-list-title">{}</h2>
-        <p className="pb-4 border-bottom">{}</p>
-        <h3>INGREDIENTS LIST</h3>
-        {/* <ul>{mapList(ingredientList)}</ul> */}
-        <table className="table">
-          <thead>
-              <tr>
-                <th>Name</th>
-                <th>Food Group</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ingredientList.map((ingredient) => (
-                <tr key={ingredient._id}>
-                  <td>{ingredient.ingredientName}</td>
-                  <td>{ingredient.foodGroup}</td>
-                  <button onClick={() => handleAddIngredient(ingredient)}>
-                    Add to User's List
-                  </button>
+      {loggedIn ? (
+        <div>
+          <article className="ingredients-list">
+            {/* INGREDIENTS LIST */}
+            <h2 className="ingredients-list-title">{}</h2>
+            <p className="pb-4 border-bottom">{}</p>
+            <h3>INGREDIENTS LIST</h3>
+            {/* <ul>{mapList(ingredientList)}</ul> */}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Food Group</th>
                 </tr>
-              ))}
-            </tbody>
-        </table>
-      </article>
-      {/* USER'S INGREDIENTS LIST */}
-      <article className="ingredients-userlist">
-        <h2 className="ingredients-userlist-title">{}</h2>
-        <p className="pb-4 border-bottom">{}</p>
-        <h2>USER'S INGREDIENTS LIST</h2>
-        <table className="table">
-          <thead>
-              <tr>
-                <th>Ingredients Selected</th>
-                <th>Ingredient Name</th>
-              </tr>
-          </thead>
-          <tbody>
-            {userIngredientList.map((ingredient) => (
-              <tr key={ingredient._id}>
-                <td>{ingredient.ingredientName}</td>
-                <td>{ingredient._id}</td>
-                <button onClick={() => handleRemoveIngredient(ingredient)}>
-                  Remove from User's List
-                </button>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </article>
-      {/* SAVE USER'S CURRENT INGREDIENTS LIST TO DB */}
-      <section>
-       <button className=" btn btn btn-primary py-2 px-4 rounded m-5" 
-          type="submit"  
-          onClick={handleSave}>
-            Save to DB
-        </button>
-      
-        <Link to="/recipe-list" className=" btn btn-primary py-2 px-4 rounded m-5">
-            Get Recipe List
-        </Link>
-      </section>
+              </thead>
+              <tbody>
+                {ingredientList.map((ingredient) => (
+                  <tr key={ingredient._id}>
+                    <td>{ingredient.ingredientName}</td>
+                    <td>{ingredient.foodGroup}</td>
+                    <button onClick={() => handleAddIngredient(ingredient)}>
+                      Add to User's List
+                    </button>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </article>
+          {/* USER'S INGREDIENTS LIST */}
+          <article className="ingredients-userlist">
+            <h2 className="ingredients-userlist-title">{}</h2>
+            <p className="pb-4 border-bottom">{}</p>
+            <h2>USER'S INGREDIENTS LIST</h2>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Ingredients Selected</th>
+                  <th>Ingredient Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userIngredientList.map((ingredient) => (
+                  <tr key={ingredient._id}>
+                    <td>{ingredient.ingredientName}</td>
+                    <td>{ingredient._id}</td>
+                    <button onClick={() => handleRemoveIngredient(ingredient)}>
+                      Remove from User's List
+                    </button>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </article>
+          {/* SAVE USER'S CURRENT INGREDIENTS LIST TO DB */}
+          <section>
+            <button
+              className=" btn btn btn-primary py-2 px-4 rounded m-5"
+              type="submit"
+              onClick={handleSave}
+            >
+              Save to DB
+            </button>
+
+            <Link
+              to="/recipe-list"
+              className=" btn btn-primary py-2 px-4 rounded m-5"
+            >
+              Get Recipe List
+            </Link>
+          </section>
+        </div>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </div>
   );
 }
 
 export default SelectIngredients;
-
-//<button className='btn btn-save' onClick={handleSave}>SAVE TO DB</button>
