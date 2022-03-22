5fc63d26ceb655f1e22837618788f062dae3995f
Bobbi.Fuller <bobbi.fuller77@gmail.com>
2022-03-21 20:18:43 -0400

    Changes Icons to Inline Block

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import AuthContext from '../context/AuthProvider';
 import { FaSearch, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 
+
 function Header() {
   // Extract the state and setter shared from the AuthContext
   const { loggedIn } = useContext(AuthContext);
@@ -23,27 +24,30 @@ function Header() {
           </li>
           <li className="nav-item">
             <NavLink to="/recipe-list" className="nav-link">
-              <FaSearch />
+              <FaSearch style={{ display: 'inline-block' }}
+ />
               Recipe List
             </NavLink>
           </li>
           <li className="nav-item">
             <NavLink to="/register" className="nav-link">
-              <FaUser />
-              Register
+              <FaUser style={{ display: 'inline-block' }}
+ />Register
             </NavLink>
           </li>
           {loggedIn ? (
             <li className="nav-item">
               <NavLink to="/logout" className="nav-link">
-                <FaSignOutAlt />
+                <FaSignOutAlt  style={{ display: 'inline-block' }}
+/>
                 Logout
               </NavLink>
             </li>
           ) : (
             <li className="nav-item">
               <NavLink to="/login" className="nav-link">
-                <FaSignInAlt />
+                <FaSignInAlt style={{ display: 'inline-block' }}
+ />
                 Login
               </NavLink>
             </li>
